9ec561b87240f441c1ae305eae34a0c5d90c6efd
Blunderproof <eafortney2@gmail.com>
2017-11-11 20:22:19 +0000

    WIP5

--- public/javascripts/sites.js
+++ public/javascripts/sites.js
@@ -1,87 +1,79 @@
 $(document).ready(function(){
   $("#createSite").click(function(){
-      var myobj = {URI:$("#uriInput").val(),bio:$("#bioInput").val(), imgURL:""};
-      jobj = JSON.stringify(myobj);
-      // $("#json").text("Your comment: " + $("#comment").val());
-      // $("#json").addClass("alert alert-success");
-      // $("#comments").removeClass("alert alert-success");
-      // $("#comments").html("");
-      var url = "submit";
-      $.ajax({
-        url:url,
-        type: "POST",
-        data: jobj,
-        contentType: "application/json; charset=utf-8",
-        success: function(data,textStatus) {
-          $.getJSON('site', function(data) {
-            var cards = "";
-            for(var site in data) {
-              info = data[site];
-              card = '
-              <div class="col-sm-3">
-                <div class="card">
-                  <div class="card-image">
-                    <img class="img-responsive" src="' + info.imgURL + '">
-                    <span class="card-title">'+ info.URI +'</span>
-                  </div>
-                  <div class="card-content">
-                    <p>' + info.bio + '</p>
-                  </div>
-                  <div class="card-action">
-                    <a href="/site/' + info.URI + '" target="new_blank">See site</a>
-                  </div>
-                </div>
-              </div>';
-              cards+=card;
+    
+    var imgString = $("#imgURLInput").val();
+    var uriString = $("#uriInput").val().toLowerCase();
+    if (imgString.match(/\.(jpeg|jpg|gif|png)$/) != null){
+      $("#formError").text("");
+      $("#formError").removeClass("alert alert-danger");
+      var check = unusedURICheck(uriString);
+      
+      if(check){ 
+        
+        var myobj = {URI: uriString, bio:$("#bioInput").val(), imgURL: imgString};
+        jobj = JSON.stringify(myobj);
+        console.log("HERE", jobj);
+        var url = "submit";
+        $.ajax({
+          url:url,
+          type: "POST",
+          data: jobj,
+          contentType: "application/json; charset=utf-8",
+          success: function(data,textStatus) {
+            $.getJSON('site', function(data) {
+              var cards = "";
+              for(var site in data) {
+                info = data[site];
+                card = '<div class="col-sm-3"><div class="card"><div class="card-image"><img class="img-responsive" src=' + info.imgURL + '><span class="card-title">'+ info.URI +'</span></div><div class="card-content"><p>' + info.bio + '</p></div><div class="card-action"><a href="/site/' + info.URI + '" target="new_blank">See site</a></div></div></div>';
+                cards+=card;
             }
-            $("#cardList").html(everything);
+              $("#cardList").html(cards);
+           })
+         }
         })
-      }
-    })
+     }else {
+       console.log("Duplicate URI string");
+       $("#formError").text("You must include a unique URI string");
+       $("#formError").addClass("alert alert-warning");
+     }
+   }else {
+    console.log("IMPROPER IMAGE URL");
+    $("#formError").text("You must include a valid image URL");
+    $("#formError").addClass("alert alert-danger");
+   }
   });
 
-  $.getJSON('site', function(data) {
-    var everything = "<ul class='list-group'>";
-    for(var site in data) {
-      info = data[site];
-      everything += "<li class='list-group-item'> URI: " + info.URI + " -- Bio: " + info.bio + "</li>";
+ function unusedURICheck(uriToSubmitString){
+  
+  var bool = true;
+   $.ajax({
+    url: "/site",
+    type: "GET",
+    dataType: 'json',
+    async: false,
+    
+    success: function(data) {
+      for(var site in data){
+        var URI = data[site].URI;
+        if(URI == uriToSubmitString){
+          bool = false;
+        }
+      }
     }
-    everything += "</ul>";
-    $("#cardList").html(everything);
   })
-
-  // $("#getComments").click(function() {
-  //   $.getJSON('comment', function(data) {
-  //     console.log(data);
-  //     var everything = "<ul class='list-group'>";
-  //     for(var comment in data) {
-  //       com = data[comment];
-  //       everything +="<li class='list-group-item'> Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
-  //     }
-  //     everything += "</ul>";
-  //     $("#comments").html(everything);
-  //     $("#json").text("");
-  //     $("#json").removeClass("alert alert-success");
-  //     $("#comments").removeClass("alert alert-success");
-  //     $("#done").text("");
-  //   })
-  // })
-
-  // $("#deleteComments").click(function() {
-  //   console.log("DELETING ALL COMMENTS");
-  //   var url = "deleteAll";
-  //
-  //   $.ajax({
-  //     url:url,
-  //     type: "GET",
-  //     success: function(data, textStatus) {
-  //       $("#comments").html("Successfully deleted all comments");
-  //       $("#comments").addClass("alert alert-success");
-  //       $("#json").text("");
-  //       $("#json").removeClass("alert alert-success");
-  //       $("#done").text("");
-  //     }
-  //   });
-  // });
+  
+  return bool;
+}
+      
+              
+   $.getJSON('site', function(data) {
+     var cards = "";
+     for(var site in data) {
+        info = data[site];
+        card = '<div class="col-sm-3"><div class="card"><div class="card-image"><img class="img-responsive" src=' + info.imgURL + '><span class="card-title">'+ info.URI +'</span></div><div class="card-content"><p>' + info.bio + '</p></div><div class="card-action"><a href="/site/' + info.URI + '" target="new_blank">See site</a></div></div></div>';
+      cards+=card;
+      }
+    $("#cardList").html(cards);
+   })
 
 });
